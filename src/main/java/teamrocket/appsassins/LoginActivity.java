00b8b8b5e5bcba862b2493a0908b0f7605352e8a
nickr77 <nickroberts57@gmail.com>
2015-10-31 17:17:15 -0500

    Added sign up, added animations to edittexts

--- src/main/java/teamrocket/appsassins/LoginActivity.java
+++ src/main/java/teamrocket/appsassins/LoginActivity.java
@@ -1,8 +1,15 @@
 package teamrocket.appsassins;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,7 +20,7 @@ import butterknife.OnClick;
 
 
 public class LoginActivity extends ActionBarActivity {
-
+    private static final String TAG = "LoginActivity";
     @Bind(R.id.loginEmail) EditText emailEntry;
     @Bind(R.id.loginPassword) EditText passwordEntry;
     @Bind(R.id.loginButton) Button loginButton;
@@ -25,17 +32,57 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        passwordEntry.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId,
+                                          KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @OnClick(R.id.loginButton)
     public void attemptLogin(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(passwordEntry.getWindowToken(), 0);
+        String email = emailEntry.getText().toString();
+        String password = passwordEntry.getText().toString();
+        if (!isEmailValid(email)) {
+            Snackbar.make(view, getString(R.string.enterValidEmail), Snackbar.LENGTH_SHORT).show();
+            emailEntry.requestFocus();
+            Log.d(TAG, email + " is invalid.");
+            return;
+        }
+        Log.d(TAG, email + " is valid!");
+        if (!verifyPassword(password)) {
+            Snackbar.make(view, getString(R.string.passwordAtLeast8) ,Snackbar.LENGTH_SHORT).show();
+            passwordEntry.requestFocus();
+            return;
+        }
+        Log.d(TAG, "Everything is valid");
+
 
     }
     @OnClick(R.id.signUpTextView)
     public void signUp(View view) {
-
+        Intent signupPage = new Intent(getApplicationContext(), Signup.class);
+        startActivity(signupPage);
     }
 
+    private boolean verifyPassword(String password) {
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        if(password.matches(regex)) {
+            return true;
+        }
+        return false;
+    }
 
+    private boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
 
 }
--- src/main/java/teamrocket/appsassins/Signup.java
+++ src/main/java/teamrocket/appsassins/Signup.java
@@ -1,39 +1,103 @@
 package teamrocket.appsassins;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class Signup extends ActionBarActivity {
+    private static final String TAG = "SignupActivity";
+    @Bind(R.id.enterFullName) EditText nameEntry;
+    @Bind(R.id.enterEmailSignup) EditText emailEntry;
+    @Bind(R.id.enterPasswordSignup) EditText passwordEntry;
+    @Bind(R.id.confirmPasswordSignup) EditText confirmPasswordEntry;
+    @Bind(R.id.signUpButton) Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+        confirmPasswordEntry.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId,
+                                          KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    signupButton.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+    @OnClick(R.id.signUpButton)
+    public void attemptSignUp(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(confirmPasswordEntry.getWindowToken(), 0);
+        String tempName = nameEntry.getText().toString();
+        String email = emailEntry.getText().toString();
+        String password = passwordEntry.getText().toString();
+        String cPass = confirmPasswordEntry.getText().toString();
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_signup, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        String[] firstLastNames = tempName.split(" ");
+        if (firstLastNames.length != 2) {
+            Snackbar.make(view, getString(R.string.errorEnterFirstandLastName) ,Snackbar.LENGTH_SHORT).show();
+            nameEntry.requestFocus();
+            return;
+        }
+        firstLastNames[1] = removeTrailingSpaces(firstLastNames[1]);
+        Log.d(TAG, "First name = " + firstLastNames[0] + " length: " + firstLastNames[0].length() + " and last name = " + firstLastNames[1] + " length: " + firstLastNames[1].length());
+        if (!isEmailValid(email)) {
+            Snackbar.make(view, getString(R.string.enterValidEmail) ,Snackbar.LENGTH_SHORT).show();
+            emailEntry.requestFocus();
+            Log.d(TAG, email + " is invalid.");
+            return;
+        }
+        Log.d(TAG, email + " is valid!");
+        if (!verifyPassword(password)) {
+            Snackbar.make(view, getString(R.string.passwordAtLeast8) ,Snackbar.LENGTH_SHORT).show();
+            passwordEntry.requestFocus();
+            return;
+        }
+        if (!cPass.equals(password)) {
+            Snackbar.make(view, getString(R.string.passDontMatch) ,Snackbar.LENGTH_SHORT).show();
+            confirmPasswordEntry.requestFocus();
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private boolean verifyPassword(String password) {
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        if(password.matches(regex)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private String removeTrailingSpaces(String word) {
+        return word.replaceAll("\\s+$", "");
+    }
+
+
+
 }
